1aa643a11b91c82fc4fc14aed563733971ecc631
Mr. SAHED ALI <142126362+iamsahed07@users.noreply.github.com>
2025-04-18 23:05:05 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,11 @@ export const Header = () => {
   return (
     <div className="fixed flex justify-between items-center h-12 bg-white shadow-lg w-full px-6">
       <div className="flex items-center">
-        <img className="w-7" src="./src/assets/icon.png" alt="icon.png" />
+        <img className="w-7" src="https://res.cloudinary.com/dosdjk5jz/image/upload/v1744997682/icon_tbsmtf.png" />
         <h1 className="ml-3 text-2xl font-bold text-[#5544D7]">DigiVoter</h1>
       </div>
       <div className="flex items-center">
-        <img className="h-6 mr-3" src="./src/assets/makaut.png" alt="Makaut Icon" />
+        <img className="h-6 mr-3" src="https://res.cloudinary.com/dosdjk5jz/image/upload/v1744997609/makaut_enzjhc.png" alt="Makaut Icon" />
         <div>
           <select className="form-select text-md">
             <option value="hn">B.TECH CSE</option>
